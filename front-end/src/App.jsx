389c5f49fe3442849e1eafe13da0a1dc05359195
bagusnaufal2 <bagusnaufalabiyyuputra@gmail.com>
2026-06-01 20:42:24 +0700

    penambahan api history

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       formData.append('resume', file);
       formData.append('jobDescription', jobDescription);
 
-      const response = await fetch(`${API_BASE_URL}/api/analyses`, {
+      const response = await fetch(`${API_BASE_URL}/api/analyze`, {
         method: 'POST',
         body: formData,
       });
